1d0c3c98ca7f8e1102dc742d37dc1bd3458f740c
phungdemachp2902 <phungdemachp2902@gmail.com>
2022-11-20 13:59:03 +0700

    add news site

---
+++ index.blade.php
@@ -0,0 +1,110 @@
+[33mcommit e8e6a35d52ffba0b6bea993b0164f38401d896fc[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m)[m
+Author: phungdemachp2902 <phungdemachp2902@gmail.com>
+Date:   Sun Nov 20 13:51:49 2022 +0700
+
+    add news site
+
+[33mcommit 6b8a581ef958633f855650040dae7f06a0265205[m[33m ([m[1;31morigin/master[m[33m, [m[1;31morigin/HEAD[m[33m)[m
+Merge: 322c5d0 f57b2c9
+Author: weed <rivenmle@gmail.com>
+Date:   Sat Nov 12 21:06:01 2022 +0700
+
+    Merge pull request #5 from weebNeedWeed/feature/show-product-data
+    
+    -feat: added pagination and show product card
+
+[33mcommit f57b2c90485209e9a922bc6547455ffda8436b30[m
+Author: Weed <rivenmle@gmail.com>
+Date:   Sat Nov 12 21:09:22 2022 +0700
+
+    -feat: added pagination and show product card
+
+[33mcommit 322c5d0e3e0be2cc487994031e81a6cd5b6684f2[m
+Merge: 2f237b7 3d3fd8c
+Author: weed <rivenmle@gmail.com>
+Date:   Thu Nov 10 01:32:52 2022 +0700
+
+    Merge pull request #4 from weebNeedWeed/feature/create-footer
+    
+    Feature/create footer
+
+[33mcommit 3d3fd8c7a9bc40c49aee4b791e0e35d1cc5f3f44[m
+Author: Weed <rivenmle@gmail.com>
+Date:   Thu Nov 10 01:36:14 2022 +0700
+
+    -feat: added favicon
+
+[33mcommit c068ccd5b0ad62b39101460c8084092285c1027b[m
+Author: Weed <rivenmle@gmail.com>
+Date:   Thu Nov 10 01:32:36 2022 +0700
+
+    -feat: added footer
+
+[33mcommit 2f237b7ecd3ce9eaf021994d5f65facfaaf39bc1[m
+Merge: bde5115 7caef9a
+Author: weed <rivenmle@gmail.com>
+Date:   Wed Nov 9 04:57:44 2022 +0700
+
+    Merge pull request #3 from weebNeedWeed/bugfix/remove-redundant-files
+    
+    Bugfix/remove redundant files
+
+[33mcommit 7caef9ab8cba812ed539b598533780aa8e24ab82[m
+Author: weed <rivenmle@gmail.com>
+Date:   Wed Nov 9 04:57:22 2022 +0700
+
+    Delete public/assets/lib/fontawesome/sprites directory
+
+[33mcommit 0d46b6ea5418472ab88d304eae1915ec4c58772a[m
+Author: weed <rivenmle@gmail.com>
+Date:   Wed Nov 9 04:57:01 2022 +0700
+
+    Delete public/assets/lib/fontawesome/metadata directory
+
+[33mcommit 731a99878a83f763acff4e74b9f6b2e80fdbe7f0[m
+Author: weed <rivenmle@gmail.com>
+Date:   Wed Nov 9 04:56:13 2022 +0700
+
+    Delete public/assets/lib/fontawesome/svgs directory
+
+[33mcommit bde511591a383f39365be6deeed2f7a100876eea[m
+Merge: b4e9e75 3738147
+Author: weed <rivenmle@gmail.com>
+Date:   Wed Nov 9 04:50:33 2022 +0700
+
+    Merge pull request #2 from weebNeedWeed/feature/setup-layout
+    
+    -feat: added ProductModel and CategoryModel
+
+[33mcommit 373814797f782d9959b9db02c107c8c742843208[m
+Author: Weed <rivenmle@gmail.com>
+Date:   Wed Nov 9 04:53:22 2022 +0700
+
+    -feat: added ProductModel and CategoryModel
+
+[33mcommit b4e9e75459b607c5a67acd4a709f68045042bf65[m
+Merge: 58827d2 3698edf
+Author: weed <rivenmle@gmail.com>
+Date:   Mon Nov 7 23:52:23 2022 +0700
+
+    Merge pull request #1 from weebNeedWeed/feature/add-stuff
+    
+    -feat: added bootstrap, jquery and popper to project
+
+[33mcommit 3698edfdfe4264b6339a11e04d4ced794bbe4367[m
+Author: Weed <rivenmle@gmail.com>
+Date:   Mon Nov 7 23:54:37 2022 +0700
+
+    -feat: added bootstrap, jquery and popper to project
+
+[33mcommit 58827d2c70db1c211635e01966d4fda2c50ce232[m
+Author: Weed <rivenmle@gmail.com>
+Date:   Sun Nov 6 21:25:45 2022 +0700
+
+    -feat: added CODEOWNERS file
+
+[33mcommit cfa17d85a5e90a17a599b7f12fa646ea880d81b4[m
+Author: Weed <rivenmle@gmail.com>
+Date:   Sun Nov 6 17:02:31 2022 +0700
+
+    Initialize Project
